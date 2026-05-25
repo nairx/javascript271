b3fcbef1e6423f8994d6938ac63e962934b38c40
nairx <nairxprv@gmail.com>
2026-05-25 14:46:49 +0530

    updated

--- index1.js
+++ index1.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+// function add(a,b){
+//     return a+b
+// }
+// const add = (a, b) => {
+//   return a + b;
+// };
+// const add = (a, b) => a + b;
+// const result = add(4, 5);
+// console.log(result);
+const sqr = a => a * a
+const result = sqr(5)
+console.log(result)
